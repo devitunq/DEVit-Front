10cb111677127569922843c507b0a2223e4d6696
Rodrigo <rodrigogarciaunq@gmail.com>
2020-09-05 06:02:12 -0300

    startup + index

---
+++ src/Components/About.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var bio = this.props.data.bio;
+    }
+
+    return (
+      <section id="about">
+      <div className="row">
+         <div className="nine columns main-col">
+            <h2>Sobre nosotros</h2>
+            <p>{bio}</p>
+            <div className="row">
+            </div>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/Components/Header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import ParticlesBg  from "particles-bg";
+
+
+class Header extends Component {
+  render() {
+
+    if(this.props.data){
+      var logopic = "images/"+this.props.data.logo;
+      var description= this.props.data.description;
+    }
+
+    return (
+      <header id="home">
+      <ParticlesBg type="circle" bg={true} />
+      <nav id="nav-wrap">
+         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" className="nav">
+            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+            <li><a className="smoothscroll" href="#about">¿Quienes somos?</a></li>
+         </ul>
+      </nav>
+
+      <div className="row banner">
+      
+         <div className="banner-text">
+            <img className="logo-pic" src={logopic} />
+            <h3>{description}.</h3>
+            <hr />
+            <ul className="social">
+               <a  className="button btn btn-primary"><i className="fa fa-play"></i>Iniciar el juego</a>
+            </ul>
+         </div>
+      </div>
+
+      <p className="scrolldown">
+         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+      </p>
+
+   </header>
+    );
+  }
+}
+
+export default Header;
