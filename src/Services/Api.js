59c23dd67dfaff9f73afa479e16f97b6b3a56aab
LM-Randomize <lcmurc@gmail.com>
2020-10-10 20:21:11 -0300

    Arreglo urls..
    Ahora a mergear master que esto se va al joraca sino..

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 
 const LEVELS_URL = "/api/levels";
+const USER_URL = "/api/user"
+const HOST_URL = "http://localHost:8080"
 
 const getLevelByLevelId = (levelId) =>
-  axios.get(`http://localHost:8080/${LEVELS_URL}/${levelId}`);
+  axios.get(`${HOST_URL}${LEVELS_URL}/${levelId}`);
 
 const getAllByDifficulty = (difficulty) =>
-  axios.get(`http://localHost:8080/${LEVELS_URL}?difficulty=${difficulty}`)
+  axios.get(`${HOST_URL}${LEVELS_URL}?difficulty=${difficulty}`)
 
 const getDifficulties = () =>
-  axios.get(`http://localHost:8080/${LEVELS_URL}/difficulties`)
+  axios.get(`${HOST_URL}${LEVELS_URL}/difficulties`)
 
 const postLevelSolution = (levelId, solution) =>
-  axios.post(`http://localHost:8080/${LEVELS_URL}/solve/${levelId}`, solution);
+  axios.post(`${HOST_URL}${LEVELS_URL}/solve/${levelId}`, solution);
 
-const getUser = (user, passowrd) =>
-  axios.post(`http://localHost:8080/${LEVELS_URL}/user`, user, passowrd);
+const getUser = (user, pass) =>
+  axios.post(`${HOST_URL}${USER_URL}/user`, {userName: user, password: pass});
 
 const getGuestPermission = () =>
-  axios.get(`http://localHost:8080/${LEVELS_URL}/guest`);
+  axios.get(`${HOST_URL}${LEVELS_URL}/guest`);
 
 export { getLevelByLevelId, postLevelSolution, getAllByDifficulty, getDifficulties };
