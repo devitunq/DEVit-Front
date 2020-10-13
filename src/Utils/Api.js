a691fd35a196ebd1628eb342284e6f667a49afe3
LM-Randomize <lcmurc@gmail.com>
2020-10-13 18:58:37 -0300

    Fix headers.
    Ahora usa axios interceptors.

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -4,19 +4,23 @@ const LEVELS_URL = "/api/levels";
 const USER_URL = "/api/user"
 const HOST_URL = "http://localHost:8080"
 
-const HEADERS = {Authorization : localStorage.getItem("accessToken")}
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+  config.headers.Authorization = token ? token : '';
+  return config
+});
 
 const getLevelByLevelId = (levelId) =>
-  axios.get(`${HOST_URL}${LEVELS_URL}/${levelId}`, {headers : HEADERS});
+  axios.get(`${HOST_URL}${LEVELS_URL}/${levelId}`);
 
 const getAllByDifficulty = (difficulty) =>
-  axios.get(`${HOST_URL}${LEVELS_URL}?difficulty=${difficulty}`, {headers : HEADERS})
+  axios.get(`${HOST_URL}${LEVELS_URL}?difficulty=${difficulty}`)
 
 const getDifficulties = () =>
-  axios.get(`${HOST_URL}${LEVELS_URL}/difficulties`, {headers : HEADERS})
+  axios.get(`${HOST_URL}${LEVELS_URL}/difficulties`);
 
 const postLevelSolution = (levelId, solution) =>
-  axios.post(`${HOST_URL}${LEVELS_URL}/solve/${levelId}`, solution, {headers : HEADERS});
+  axios.post(`${HOST_URL}${LEVELS_URL}/solve/${levelId}`, solution);
 
 const getUser = (user, pass) =>
   axios.post(`${HOST_URL}${USER_URL}`, {userName: user, password: pass});
