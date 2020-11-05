00fb38e67b309f27609a02c743afcd3c6052ccf6
Rodrigo <rodrigogarciaunq@gmail.com>
2020-11-05 19:03:47 -0300

    merged branchg

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,88 +1,13 @@
 import React, { useState, useEffect } from 'react';
-<<<<<<< HEAD
-import { Grid, LinearProgress } from '@material-ui/core';
-=======
 import ParticlesBg from "particles-bg";
-import { Grid } from '@material-ui/core';
+import { Grid, LinearProgress } from '@material-ui/core';
 import LockIcon from '@material-ui/icons/Lock';
->>>>>>> e3d410e9522c1f8bcf364355848663208e6b14b8
 import '../Pages/styles/Profile.css'
 import LevelProgress from '../Components/Profile/LevelsProgress';
 import ProfileItem from '../Components/Profile/ProfileItem';
 import ProfileHeader from '../Components/Profile/ProfileHeader';
 import { getUserCompletionProgress } from '../Utils/Api';
 import Navbar from '../Components/Generics/Navbar'
-<<<<<<< HEAD
-import ParticlesBg from "particles-bg";
-import LockIcon from '@material-ui/icons/Lock';
-
-const Profile = () => {
-
-  const [completionProgress, setCompletionProgress] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (isLoading) {
-      getUserCompletionProgress(localStorage.getItem("userName")).then((response) => {
-        console.log(response)
-        setCompletionProgress(response);
-        setLoading(false);
-      }).catch(e => console.log(e))
-    };
-  });
-
-  return isLoading ? (
-    <LinearProgress variant="indeterminate" />
-  ) : (
-      <div className="profile-container">
-
-        <ParticlesBg type="circle" bg={true} />
-
-        <Navbar />
-
-        <div className="profile-cont">
-          <Grid container direction="row" spacing={8}>
-            <ProfileHeader />
-
-            <Grid container spacing={8}>
-              <Grid item xs={8}>
-
-                <Grid item xs={12}>
-                  <Grid container spacing={8}>
-                    <ProfileItem
-                      detailTitle="Nombre de usuario: "
-                      detail={localStorage.getItem("userName")}
-                    />
-                    <ProfileItem
-                      detailTitle={"Nick: "}
-                      detail={localStorage.getItem("nick")}
-                    />
-                    <ProfileItem
-                      icon={<LockIcon
-                        style={{
-                          width: "20%",
-                          marginTop: "1%",
-                          marginLeft: "10%",
-                        }}
-                      />}
-                      detailTitle={"Contraseña: "}
-                      detail="********"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Grid container spacing={8}>
-                      <LevelProgress
-                        progressBarTitle="Porcentaje de juego completado: "
-                        value={completionProgress}
-                        min={0}
-                        max={100}
-                      />
-                    </Grid>
-                  </Grid>
-
-
-=======
 import { getCompletionProgress } from "../Utils/Api";
 
 const Profile = () => {
@@ -124,7 +49,6 @@ const Profile = () => {
                     detailTitle={"Contraseña: "}
                     detail="********"
                   />
->>>>>>> e3d410e9522c1f8bcf364355848663208e6b14b8
                 </Grid>
                 <Grid item xs={12}>
                   <Grid container spacing={8}>
@@ -137,15 +61,6 @@ const Profile = () => {
                   </Grid>
                 </Grid>
               </Grid>
-<<<<<<< HEAD
-
-            </Grid>
-          </Grid>
-        </div >
-      </div >
-    )
-};
-=======
             </Grid>
           </Grid>
         </Grid>
@@ -153,6 +68,5 @@ const Profile = () => {
     </div >
   );
 }
->>>>>>> e3d410e9522c1f8bcf364355848663208e6b14b8
 
 export default Profile;
